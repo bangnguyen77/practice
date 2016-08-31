2104b4f5dc4b808cb6d1b34a9d22ea1d65498e6a
Bang Nguyen <bangnv77@gmail.com>
2016-08-30 17:28:03 -0700

    create add-topic component and allow users to add new topic on the index.hbs

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('topic');
+  },
+
+  actions: {
+    saveTopic(params) {
+      var newTopic = this.store.createRecord('topic', params);
+      newTopic.save();
+      this.transitionTo('index');
+    }
   }
 });
